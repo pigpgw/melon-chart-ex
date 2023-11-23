2e32ee1b1b6e8618a98f08192b1b3dec40dc495f
pigpgw <ceh20002@naver.com>
2023-11-23 13:39:31 +0900

    Docs 고민사항 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState , useEffect } from 'react'
 import { FaHeart } from "react-icons/fa6";
 import './App.css'
 
-
 function App() {
 
   const [isLoading, setIsLoading] = useState(false);
